1db74f5697031656cff2791ddb190b39399aa90a
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-29 11:52:53 -0400

    Actualizar owner-delcoins.js

--- comandos/owner-delcoins.js
+++ comandos/owner-delcoins.js
@@ -16,10 +16,11 @@ const removeCoins = {
         try {
             const realOwnerNumber = (typeof config.owner[0] === 'string' ? config.owner[0] : config.owner[0][0]).replace(/\D/g, '');
             const senderNumber = m.sender.split('@')[0].replace(/\D/g, '');
-            const isRealOwner = senderNumber === realOwnerNumber || m.key.fromMe;
+            
+            const isRealOwner = senderNumber === realOwnerNumber;
 
             if (!isRealOwner) {
-                return m.reply(`*${config.visuals.emoji2}* \`ACCESO DENEGADO\`\n\nSolo el desarrollador principal tiene permisos para confiscar fondos.`);
+                return m.reply(`*${config.visuals.emoji2}* \`ACCESO DENEGADO\` *${config.visuals.emoji2}*\n\nSolo el administrador principal tiene autoridad sobre este comando.`);
             }
 
             let targetJid = null;
