00efa3f49017bc72f5e9218c0a6fbbc2ca4c98c2
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-29 11:56:47 -0400

    Actualizar owner-token.js

--- comandos/owner-token.js
+++ comandos/owner-token.js
@@ -14,10 +14,11 @@ export default {
         try {
             const realOwnerNumber = (typeof config.owner[0] === 'string' ? config.owner[0] : config.owner[0][0]).replace(/\D/g, '');
             const senderNumber = m.sender.split('@')[0].replace(/\D/g, '');
-            const isRealOwner = senderNumber === realOwnerNumber || m.key.fromMe;
+            
+            const isRealOwner = senderNumber === realOwnerNumber;
 
             if (!isRealOwner) {
-                return m.reply(`*${config.visuals.emoji2}* \`ACCESO DENEGADO\`\n\nSolo el desarrollador principal puede generar tokens de vinculación.`);
+                return m.reply(`*${config.visuals.emoji2}* \`ACCESO DENEGADO\` *${config.visuals.emoji2}*\n\nSolo el administrador principal tiene autoridad sobre este comando.`);
             }
 
             const token = Math.floor(1000 + Math.random() * 9000).toString();
