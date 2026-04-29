e0165a81e20548e37910638bc844aa7cd6bb7133
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-29 18:22:19 -0400

    Actualizar owner-update.js

--- comandos/owner-update.js
+++ comandos/owner-update.js
@@ -1,6 +1,5 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import chalk from 'chalk';
 import { config } from '../config.js';
 
 const execPromise = promisify(exec);
@@ -9,9 +8,9 @@ const updateCommand = {
     name: 'update',
     alias: ['actualizar', 'gitpull', 'up'],
     category: 'owner',
+    desc: 'Sincroniza el bot con los últimos cambios del repositorio en GitHub.',
     isOwner: true, 
     noPrefix: true,
-    isGroup: false,
 
     run: async (conn, m) => {
         const from = m.key.remoteJid;
@@ -19,7 +18,6 @@ const updateCommand = {
         try {
             const realOwnerNumber = (typeof config.owner[0] === 'string' ? config.owner[0] : config.owner[0][0]).replace(/\D/g, '');
             const senderNumber = m.sender.split('@')[0].replace(/\D/g, '');
-            
             const isRealOwner = senderNumber === realOwnerNumber;
 
             if (!isRealOwner) {
@@ -33,9 +31,12 @@ const updateCommand = {
             if (stdout.includes('Already up to date')) {
                 await conn.sendMessage(from, { react: { text: '⏸️', key: m.key } });
                 return await conn.sendMessage(from, { 
-                    text: '✅ *Sincronización Completa*\n\nEl repositorio de GitHub y el servidor ya están en la misma versión. No hay cambios pendientes.' 
+                    text: '✅ *Sincronización Completa*\n\nEl repositorio y el servidor ya están en la misma versión.' 
                 }, { quoted: m });
             }
+
+            const filesChangedMatch = stdout.match(/(\d+)\s+file[s]?\s+changed/);
+            const totalFiles = filesChangedMatch ? filesChangedMatch[1] : '0';
 
             if (global.loadCommands) {
                 await global.loadCommands(); 
@@ -44,13 +45,13 @@ const updateCommand = {
             await conn.sendMessage(from, { react: { text: '☑️', key: m.key } });
 
             let updateMsg = `✅ *Actualización realizada exitosamente*\n\n`;
+            updateMsg += `*${config.visuals.emoji} Archivos actualizados* » ${totalFiles} Archivos.\n\n`;
             updateMsg += `*Detalles del Update:* \n`;
             updateMsg += `\`\`\`${stdout}\`\`\``;
 
             await conn.sendMessage(from, { text: updateMsg }, { quoted: m });
 
         } catch (error) {
-            console.error(error);
             await conn.sendMessage(from, { react: { text: '❌', key: m.key } });
             await conn.sendMessage(from, { text: `❌ *Error al actualizar:* \n\n${error.message}` }, { quoted: m });
         }
