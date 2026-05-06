e926fd38282181e2bb428ae795379dab32ce57cf
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-06 19:19:10 -0400

    Actualizar descargas-ttsearch.js

--- comandos/descargas-ttsearch.js
+++ comandos/descargas-ttsearch.js
@@ -14,7 +14,7 @@ const tiktokSearch = {
         await conn.sendMessage(m.chat, { react: { text: '⌛', key: m.key } });
 
         try {
-            const { data: res } = await axios.get(`https://api.kazuma.giize.com/api/search/tiktok?query=${encodeURIComponent(text)}&apikey=${config.apiKzm}`);
+            const { data: res } = await axios.get(`https://rest.kazuma.giize.com/api/search/tiktok?query=${encodeURIComponent(text)}&apiKey=${config.apiKzm}`);
 
             if (!res.status || !res.data?.length) {
                 await conn.sendMessage(m.chat, { react: { text: '❌', key: m.key } });
