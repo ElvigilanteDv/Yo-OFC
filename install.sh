72da72c5ba6f1c051113485777fbaef64319d67b
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-07 09:41:49 -0400

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -4,41 +4,48 @@ export DEBIAN_FRONTEND=noninteractive
 
 clear
 echo -e "\e[1;36m┏━━━━✿︎ Kazuma-Mr-Bot ✿︎━━━━╮\e[0m"
-echo -e "\e[1;36m┃ \e[0m\e[1;33m✐ Iniciando Instalador Automático\e[0m"
-echo -e "\e[1;36m┃ \e[0m\e[1;32m✐ Aplicando Modo Salto de Errores...\e[0m"
+echo -e "\e[1;36m┃ \e[0m\e[1;33m✐ Instalador Universal Pro\e[0m"
+echo -e "\e[1;36m┃ \e[0m\e[1;32m✐ Pterodactyl • VPS • Termux\e[0m"
 echo -e "\e[1;36m╰━━━━━━━━━━━━━━━━━━━╯\e[0m"
 echo ""
 
-echo -e "\e[1;34m[1/6] Configurando almacenamiento...\e[0m"
-termux-setup-storage -y
-sleep 2
-
-echo -e "\e[1;34m[2/6] Reparando base de datos de paquetes...\e[0m"
-pkg fix-missing -y
-dpkg --configure -a
-
-echo -e "\e[1;34m[3/6] Instalando binarios necesarios...\e[0m"
-pkg install git nodejs python ffmpeg libwebp -y
-
-echo -e "\e[1;34m[4/6] Entrando al repositorio...\e[0m"
-if [ -d "Kazuma-Mr-Bot" ]; then
-  cd Kazuma-Mr-Bot
+# 1. Detección de Entorno
+IS_TERMUX=false
+if [[ $(command -v termux-setup-storage) ]]; then
+    IS_TERMUX=true
+    echo -e "\e[1;34m[!] Entorno Termux detectado. Aplicando optimizaciones...\e[0m"
+    termux-setup-storage -y
+    pkg fix-missing -y
+    dpkg --configure -a
+    pkg install git nodejs ffmpeg libwebp -y
+else
+    echo -e "\e[1;34m[!] Entorno Linux/Pterodactyl detectado.\e[0m"
 fi
 
-echo -e "\e[1;34m[5/6] Instalando dependencias (Omitiendo Binarios Conflictivos)...\e[0m"
+# 2. Instalación de Dependencias
+echo -e "\e[1;34m[*] Instalando módulos de Node.js...\e[0m"
 rm -rf node_modules package-lock.json
-# Esta línea es la que salvará la instalación:
-npm config set ignore-scripts true
-npm install --no-bin-links
 
-echo -e "\e[1;34m[6/6] Instalando Baileys manualmente...\e[0m"
-npm install @whiskeysockets/baileys pino qrcode-terminal
+if [ "$IS_TERMUX" = true ]; then
+    # Evita que librerías gráficas pesadas rompan la instalación en móvil
+    npm config set ignore-scripts true
+    npm install --no-bin-links
+else
+    npm install
+fi
 
+# 3. EL PARCHE MAESTRO (Compatibilidad Sharp)
+# Esto busca cualquier intento de cargar 'sharp' y lo envuelve en un try-catch
+# para que el bot no muera si la librería falla en Android.
+echo -e "\e[1;34m[*] Aplicando parches de compatibilidad...\e[0m"
+find . -type f -name "*.js" -exec sed -i "s/require('sharp')/eval('try { require(\"sharp\") } catch (e) { null }')/g" {} + 2>/dev/null
+
+# 4. Finalización
 clear
-echo -e "\e[1;32m┏━━━━✿︎ INSTALACIÓN FINALIZADA ✿︎━━━━╮\e[0m"
-echo -e "\e[1;32m┃ \e[0m\e[1;33m✐ El bot iniciará omitiendo librerías pesadas.\e[0m"
-echo -e "\e[1;32m┃ \e[0m\e[1;32m✐ Disfruta de Kazuma en Termux.\e[0m"
+echo -e "\e[1;32m┏━━━━✿︎ INSTALACIÓN COMPLETADA ✿︎━━━━╮\e[0m"
+echo -e "\e[1;32m┃ \e[0m\e[1;33m✐ El bot está configurado para este sistema.\e[0m"
+echo -e "\e[1;32m┃ \e[0m\e[1;32m✐ Iniciando en 3 segundos...\e[0m"
 echo -e "\e[1;32m╰━━━━━━━━━━━━━━━━━━━━━━╯\e[0m"
-echo ""
+sleep 3
 
 npm start
