fd347240313249b76bbdba5406dd1f1174ad0264
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 14:31:34 -0400

    Actualizar profile-setage.js

--- comandos/profile-setage.js
+++ comandos/profile-setage.js
@@ -1,8 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import { config } from '../config.js';
-
-const dbPath = path.resolve('./config/database/profile/birthdays.json');
 
 const setAge = {
     name: 'setage',
@@ -13,21 +9,24 @@ const setAge = {
 
     run: async (conn, m, args) => {
         try {
-            const user = m.sender.split('@')[0].split(':')[0];
-            if (!fs.existsSync(dbPath)) fs.writeFileSync(dbPath, JSON.stringify({}));
-            let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+            const userJid = m.sender.replace(/:.*@/g, '@');
+            if (!global.db.data.users[userJid]) global.db.data.users[userJid] = {};
+            const userDb = global.db.data.users[userJid];
 
-            if (!args[0]) return m.reply(`*${config.visuals.emoji2} \`FALTAN DATOS\` ${config.visuals.emoji2}*\n\nUso: #setage [número]\n\n> ¡Define tu etapa actual!`);
+            if (!args[0]) return m.reply(`*${config.visuals.emoji2} \`FALTAN DATOS\` ${config.visuals.emoji2}*\n\nUso: #setage [número]`);
 
             const age = parseInt(args[0]);
             if (isNaN(age) || age < 8 || age > 85) return m.reply(`*${config.visuals.emoji2} \`RANGO EXCEDIDO\` ${config.visuals.emoji2}*\n\nSolo de 8 a 85 años.`);
 
             const estimatedYear = 2026 - age;
-            db[user] = { birth: `01/01/${estimatedYear}`, age: age };
-            fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
+            userDb.birthday = { 
+                date: `01/01/${estimatedYear}`, 
+                age: age 
+            };
 
             m.reply(`*${config.visuals.emoji3} \`EDAD REGISTRADA\` ${config.visuals.emoji3}*\n\nEdad: *${age} años*\n\n> ¡Tu perfil ha sido actualizado!`);
         } catch (e) {
+            console.error(e);
             m.reply('✘ Error al procesar edad.');
         }
     }
