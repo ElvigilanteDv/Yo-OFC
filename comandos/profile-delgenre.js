c8ffa68238356c4440c71591a43b1f27330c4f31
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 14:28:20 -0400

    Actualizar profile-delgenre.js

--- comandos/profile-delgenre.js
+++ comandos/profile-delgenre.js
@@ -1,8 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import { config } from '../config.js';
-
-const genrePath = path.resolve('./config/database/profile/genres.json');
 
 const delGenre = {
     name: 'delgenre',
@@ -13,16 +9,18 @@ const delGenre = {
 
     run: async (conn, m) => {
         try {
-            const user = m.sender.split('@')[0].split(':')[0];
-            if (!fs.existsSync(genrePath)) return m.reply(`*${config.visuals.emoji2}* Sin registros.`);
-            
-            let genres = JSON.parse(fs.readFileSync(genrePath, 'utf-8'));
-            if (!genres[user]) return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo posees un género establecido para borrar.\n\n> ¡Usa #setgenre para registrarte!`);
+            const userJid = m.sender.replace(/:.*@/g, '@');
+            const userDb = global.db.data.users[userJid];
 
-            delete genres[user];
-            fs.writeFileSync(genrePath, JSON.stringify(genres, null, 2));
+            if (!userDb || !userDb.genre) {
+                return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo posees un género establecido para borrar.\n\n> ¡Usa #setgenre para registrarte!`);
+            }
+
+            delete userDb.genre;
+
             m.reply(`*${config.visuals.emoji3} \`GÉNERO PURGADO\` ${config.visuals.emoji3}*\n\nTu identidad ha sido eliminada de la base de datos 🗑️\n\n> ¡Puedes volver a elegir un género cuando gustes!`);
         } catch (e) {
+            console.error(e);
             m.reply('✘ Error al purgar identidad.');
         }
     }
