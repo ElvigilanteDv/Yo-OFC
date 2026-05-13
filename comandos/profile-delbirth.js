8b983153bbacbf039228be5a67b51f3382ed3902
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 14:27:18 -0400

    Actualizar profile-delbirth.js

--- comandos/profile-delbirth.js
+++ comandos/profile-delbirth.js
@@ -1,8 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import { config } from '../config.js';
-
-const dbPath = path.resolve('./config/database/profile/birthdays.json');
 
 const delBirth = {
     name: 'delbirth',
@@ -13,16 +9,18 @@ const delBirth = {
 
     run: async (conn, m) => {
         try {
-            const user = m.sender.split('@')[0].split(':')[0];
-            if (!fs.existsSync(dbPath)) return m.reply(`*${config.visuals.emoji2}* Sin registros.`);
-            let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+            const userJid = m.sender.replace(/:.*@/g, '@');
+            const userDb = global.db.data.users[userJid];
 
-            if (!db[user]) return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo hay fecha para borrar.`);
+            if (!userDb || !userDb.birthday) {
+                return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo hay fecha para borrar.`);
+            }
 
-            delete db[user];
-            fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
+            delete userDb.birthday;
+
             m.reply(`*${config.visuals.emoji3} \`REGISTRO PURGADO\` ${config.visuals.emoji3}*\n\nFecha eliminada.\n\n> ¡Has vuelto a ser un ser sin tiempo!`);
         } catch (e) {
+            console.error(e);
             m.reply('✘ Error al purgar cronología.');
         }
     }
