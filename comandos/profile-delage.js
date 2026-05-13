888deada6ee132e41164c97ca726bbac6da80fde
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 14:26:58 -0400

    Actualizar profile-delage.js

--- comandos/profile-delage.js
+++ comandos/profile-delage.js
@@ -1,8 +1,4 @@
-import fs from 'fs';
-import path from 'path';
 import { config } from '../config.js';
-
-const dbPath = path.resolve('./config/database/profile/birthdays.json');
 
 const delAge = {
     name: 'delage',
@@ -13,16 +9,18 @@ const delAge = {
 
     run: async (conn, m) => {
         try {
-            const user = m.sender.split('@')[0].split(':')[0];
-            if (!fs.existsSync(dbPath)) return m.reply(`*${config.visuals.emoji2}* Sin registros.`);
-            let db = JSON.parse(fs.readFileSync(dbPath, 'utf-8'));
+            const userJid = m.sender.replace(/:.*@/g, '@');
+            const userDb = global.db.data.users[userJid];
 
-            if (!db[user]) return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo hay edad registrada.`);
+            if (!userDb || !userDb.birthday) {
+                return m.reply(`*${config.visuals.emoji2} \`DATO INEXISTENTE\` ${config.visuals.emoji2}*\n\nNo hay edad registrada.`);
+            }
 
-            delete db[user];
-            fs.writeFileSync(dbPath, JSON.stringify(db, null, 2));
+            delete userDb.birthday;
+            
             m.reply(`*${config.visuals.emoji3} \`EDAD PURGADA\` ${config.visuals.emoji3}*\n\nEdad eliminada del registro.\n\n> ¡Vuelve a ser joven eternamente!`);
         } catch (e) {
+            console.error(e);
             m.reply('✘ Error al eliminar edad.');
         }
     }
