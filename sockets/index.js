317076e71b69368cccd098b89b743051024ecc96
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 20:12:38 -0400

    Actualizar index.js

--- sockets/index.js
+++ sockets/index.js
@@ -7,7 +7,7 @@ import {
     Browsers,
     jidNormalizedUser,
     downloadMediaMessage 
-} from '@whiskeysockets/baileys';
+} from 'todleys';
 import P from 'pino';
 import fs from 'fs';
 import path from 'path';
